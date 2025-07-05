6aa95a02937a22b615f3738bf66bedcb3fe5e33a
dntjr8989 <dntjr8989@naver.com>
2025-07-05 16:44:40 +0900

    first commit

---
+++ src/main/java/com/kws/lostarkhelper/LostarkhelperApplication.java
@@ -0,0 +1,13 @@
+package com.kws.lostarkhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostarkhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kws/lostarkhelper/LostarkhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kws.lostarkhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
