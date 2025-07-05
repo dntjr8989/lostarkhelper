c8db30e40c4608e439ab59709b62434d76afa5d4
dntjr8989 <dntjr8989@naver.com>
2025-07-05 17:15:36 +0900

    LostAPI 연결
    우선 RestTemplate이 아닌 RestClient를 이용해 LostAPI JWT 토큰으로
    캐릭터 정보를 조회하는 api를 테스트 해봤다. RestClient는 동기식 HTTP
    Client이며, WebClient의 Fluent API 방식에 RestTemplate와 같은
    인프라스트럭처인 특징이 있다고 한다. FluentAPI란 메소드 체이닝을 활용해
    코드 가독성을 높이는 프로그래밍기법

--- src/main/java/com/kws/lostarkhelper/LostarkhelperApplication.java
+++ src/main/java/com/kws/lostarkhelper/LostarkhelperApplication.java
@@ -1,13 +1,16 @@
 package com.kws.lostarkhelper;
 
+import com.kws.lostarkhelper.lostarkapi.LostArkAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LostarkhelperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LostarkhelperApplication.class, args);
+		LostArkAPI lostArkAPI = new LostArkAPI();
+		lostArkAPI.callAPI();
+		//SpringApplication.run(LostarkhelperApplication.class, args);
 	}
 
 }
